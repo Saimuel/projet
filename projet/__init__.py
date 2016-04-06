68604311ef19c2de7e2b7607ce95306208370012
unknown <samuel.veillette@gmail.com>
2016-04-06 19:16:58 -0400

    rito

--- projet/__init__.py
+++ projet/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_pyfile("config.py")
+db = SQLAlchemy(app)
 
-import projet.views
+import projet.views, projet.models
