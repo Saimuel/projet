686e8ba2d5051c2f4f71399249d7cb792e778960
Samuel Veillette <SamuelVeillette@Samuels-MacBook-Pro.local>
2016-04-07 23:44:10 -0400

    good night sweet prince

--- projet/models.py
+++ projet/models.py
@@ -14,3 +14,36 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+class Property(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String(80), unique=True)
+
+    def __init__(self, address):
+        self.address = address
+
+    def __repr__(self):
+        return '<User %r>' % self.address
+
+
+class Tenant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
+
+class Payment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Integer)
+
+    def __init__(self, amount):
+        self.amount = amount
+
+    def __repr__(self):
+        return '<Payment %r>' % self.id
