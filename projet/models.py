762bd40d4a5aec20d79d4461632f7ea9ba8a62be
Samuel Veillette <SamuelVeillette@Samuels-MacBook-Pro.local>
2016-04-07 23:23:32 -0400

    test

--- projet/models.py
+++ projet/models.py
@@ -5,10 +5,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(255))
 
-    def __init__(self, username, email):
+    def __init__(self, username, email, password):
         self.username = username
         self.email = email
+        self.password = password
 
     def __repr__(self):
         return '<User %r>' % self.username
