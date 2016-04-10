d5f50525af2ab9cf07bf250ce175001191e78823
unknown <samuel.veillette@gmail.com>
2016-04-10 18:45:00 -0400

    hello

--- projet/views.py
+++ projet/views.py
@@ -1,6 +1,13 @@
 from projet import app
-from flask import render_template
+from flask import render_template, request
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template("login.html")
